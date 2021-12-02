ffe5e6b7cbaa3d6e3d4163fdad1493a11eda7457
AshishKumarJena <ashishkujena2000@gmail.com>
2021-12-02 14:11:37 +0530

    removed static file storage

--- job/settings.py
+++ job/settings.py
@@ -177,5 +177,5 @@ MESSAGE_TAGS = {
 # Activate Django-Heroku.
 django_heroku.settings(locals())
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
